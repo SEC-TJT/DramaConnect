5ac5cc40f283481334449322642ba4671b33863b
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-04-17 08:52:44 +0800

    fix typo add env_spec

--- spec/unit/drama_spec.rb
+++ spec/unit/drama_spec.rb
@@ -7,13 +7,13 @@ describe 'Test Drama Handling' do
     wipe_database
 
     DATA[:dramalists].each do |drama_list|
-      DramaConnect::DramaList.create(drama_list)
+      DramaConnect::Dramalist.create(drama_list)
     end
   end
 
   it 'HAPPY: should retrieve correct data from database' do
     drama_data = DATA[:dramas][1]
-    drama_list = DramaConnect::DramaList.first
+    drama_list = DramaConnect::Dramalist.first
     new_drama = drama_list.add_drama(drama_data)
 
     drama = DramaConnect::Drama.find(id: new_drama.id)
@@ -23,7 +23,7 @@ describe 'Test Drama Handling' do
 
   it 'SECURITY: should not use deterministic integers' do
     drama_data = DATA[:dramas][1]
-    drama_list = DramaConnect::DramaList.first
+    drama_list = DramaConnect::Dramalist.first
     new_drama = drama_list.add_drama(drama_data)
 
     _(new_drama.id.is_a?(Numeric)).must_equal false
@@ -31,7 +31,7 @@ describe 'Test Drama Handling' do
 
   it 'SECURITY: should secure sensitive attributes' do
     drama_data = DATA[:dramas][1]
-    drama_list = DramaConnect::DramaList.first
+    drama_list = DramaConnect::Dramalist.first
     new_drama = drama_list.add_drama(drama_data)
     stored_drama = app.DB[:dramas].first
 
