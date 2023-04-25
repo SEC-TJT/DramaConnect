d5c59d69783ee105d91fd3fcf486a6e0f4bf4ab4
a0716116z <65119444+a0716116z@users.noreply.github.com>
2023-04-25 12:00:53 +0800

    fix typo

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,10 +9,14 @@ require 'yaml'
 require_relative 'test_load_all'
 
 def wipe_database
-  app.DB[:dramas].delete
-  app.DB[:dramalists].delete
+  # app.DB[:dramas].delete
+  # app.DB[:dramalists].delete
+  DramaConnect::Drama.map(&:destroy)
+  DramaConnect::Dramalist.map(&:destroy)
+  DramaConnect::Account.map(&:destroy)
 end
 
 DATA = {} # rubocop:disable Style/MutableConstant
 DATA[:dramas] = YAML.safe_load File.read('app/db/seeds/drama_seeds.yml')
 DATA[:dramalists] = YAML.safe_load File.read('app/db/seeds/dramalist_seeds.yml')
+DATA[:accounts] = YAML.safe_load File.read('app/db/seeds/account_seeds.yml')
